3dc8095de06f841e1d2796bd6c471bb14070d0d5
daniyarky <danik.kuanyshbayev@gmail.com>
2022-11-06 19:15:31 +0600

    task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a=1;
+        float b=0;
+        float formulaValue=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
     }
 }
